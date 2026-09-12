9c2e4ac331ab7c8bfc7063be43650c76d038452d
Alfred Lipset <alfredhome@alfreds-mini.mynetworksettings.com>
2026-09-11 23:24:10 -0500

    Restore featured Instagram post embed

--- schedule-config.js
+++ schedule-config.js
@@ -13,7 +13,7 @@ window.SPA_SCHEDULE_CONFIG = {
   instagramProfileUrl: "https://www.instagram.com/spa_boyssoccer/",
   // Optional: add a supported embed/widget URL here for an inline feed.
   // A normal Instagram profile URL usually cannot be embedded directly.
-  instagramEmbedUrl: "",
+  instagramEmbedUrl: "https://www.instagram.com/p/DcuXRJ4RNGa/",
   stats: {
     updated: "Sep 11, 2026",
     team: [
